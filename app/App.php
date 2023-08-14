7aed1794338674873a0319d77d91788f9464b2cf
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-08-14 22:32:49 +0700

    add 404.php and implement url handle in App.php

--- app/App.php
+++ app/App.php
@@ -2,9 +2,76 @@
 
 class App {
 
-	public function __construct () {
+	private $__controller, $__action, $__params;
 
-		echo '<h1>It works</h1>';
+	function __construct() {
+
+		global $routes;
+
+		$this->__controller = $routes['default_controller'];
+		$this->__action = $routes['default_action'];
+		$this->__params = [];
+
+		$this->handleUrl();
+	}
+	
+	function getUrl() {
+		if(!empty($_SERVER['PATH_INFO'])) {
+			$url = $_SERVER['PATH_INFO'];
+		} else {
+			$url = '/';
+		}
+		return $url;
+	}
+//locahost/product/detail/1/1/2 /controller/action/params
+	public function handleUrl() {
+
+		$url = $this->getUrl();
+		$urlArr = array_filter(explode('/', $url));
+		$urlArr = array_values($urlArr);
+
+		if (strcmp($urlArr[0], 'signup') == 0) {
+			$this->__controller = 'Signup';
+			
+
+		} elseif (strcmp($urlArr[0], 'login') == 0) {
+			$this->__controller = 'Login';
+
+		} elseif(!empty($urlArr[0])) {
+			$this->__controller = ucfirst($urlArr[0]);
+
+		} else {
+			
+		}
+		if(file_exists('app/controllers/'.($this->__controller).'.php')) {
+			require_once 'app/controllers/'.($this->__controller).'.php';
+			$this->__controller = new $this->__controller();
+		} else {
+			$this->loadError();
+		}
+		unset($urlArr[0]);
+
+		if (!empty($urlArr[1])) {
+			$this->__action = $urlArr[1];
+
+		} else {
+
+			$this->__action = 'index';
+		}
+
+		unset($urlArr[1]);
+
+		$this->__params = array_values($urlArr);
+
+		if(method_exists($this->__controller, $this->__action)) {
+
+			call_user_func_array([$this->__controller, $this->__action], $this->__params);
+		} else {
+			$this->loadError();
+		}
+	}
+
+	public function loadError($name = '404') {
+		require_once 'errors/'.$name.'.php';
 	}
 }
-
