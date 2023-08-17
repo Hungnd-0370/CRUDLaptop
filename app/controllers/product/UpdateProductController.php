de9acadd04b0ffe6b8d3eac6431eca3572cc580c
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-08-17 12:39:20 +0700

    refactor UpdateController

--- app/controllers/product/UpdateProductController.php
+++ app/controllers/product/UpdateProductController.php
@@ -2,35 +2,47 @@
 require_once __DIR_ROOT.'/core/Controller.php';
 require_once __DIR_ROOT.'/app/models/product/mappers/ProductMapper.php';
 require_once __DIR_ROOT.'/helpers/session_helper.php';
+require_once __DIR_ROOT.'/helpers/validate.php';
 require_once __DIR_ROOT.'/app/models/product/entities/Product.php';
+
 class UpdateProductController extends Controller{
+
     private $productMapper;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->productMapper = new ProductMapper();
     }
+
     public function openModal($id){
+
         $product = $this->productMapper->getProductDetail($id);
 
 		if (!empty($product)){
+
             $data = [];
             $data['subContent']['productInfo'] = $product;
             $data['content'] = 'product/modify';
 
             $this->render('layouts/layout', $data);
-        }else require_once __DIR_ROOT.'/app/errors/404.php';
+
+        } else {
+			require_once __DIR_ROOT.'/app/errors/404.php';
+		}
     }
     public function update(){
-        $product = new Product(trim($_POST['id']), trim($_POST['name']), trim($_POST['version']),
-            trim($_POST['color']), trim($_POST['price']), trim($_POST['description']));
 
-        if(empty($product->getProductId()) || empty($product->getProductName()) || empty($product->getProductVersion()) ||
-            empty($product->getProductColor()) || empty($product->getProductPrice()) || empty($product->getProductDescription())){
-            flash("updateProduct", "Please fill out all inputs");
-            redirect(_WEB_ROOT.'/products');
-            exit();
-        }
+		$productId = trim($_POST['id']);
+		$productName = trim($_POST['name']);
+		$productVersion = trim($_POST['version']);
+		$productColor = trim($_POST['color']);
+		$productPrice = trim($_POST['price']);
+		$productDescription = trim($_POST['description']);
+		
+		if(!arrayEmptyValidate([$productId, $productName, $productVersion, $productColor, $productPrice, $productDescription])){
+			$this->anyElementEmptyNotify();
+		}
+		
+        $product = new Product($productId, $productName, $productVersion, $productColor, $productPrice, $productDescription);
 
         if ($this->productMapper->updateProduct($product)){
             redirect(_WEB_ROOT.'/products');
@@ -38,6 +50,12 @@ class UpdateProductController extends Controller{
             die("Something went wrong");
         }
     }
+
+	public function anyElementEmptyNotify () {
+		flash("updateProduct", "Please fill out all inputs");
+		redirect(_WEB_ROOT.'/products');
+		exit();
+	}
 }
 
 ?>
